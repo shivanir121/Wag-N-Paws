b92b1fcd052f9e5d49c2297689abe9f60f567291
SHIVANI RAJPUT <shivanir121@gmail.com>
2019-12-26 21:21:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+//on page move animation
+sr.reveal('.leftAnimation', {
+    origin: 'left',
+});
+sr.reveal('.RightAnimation', {
+    origin: 'right',
+});
+
+sr.reveal('.bottomAnimation', {
+    origin: 'bottom',
+});
+
+sr.reveal('.topAnimation', {
+    origin: 'top',
+});
+
+
